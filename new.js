aaf32fa62275e105cbc32886a5d09b37f8ef159d
f-endev <nankanikushal@gmail.com>
2025-07-21 10:44:00 -0700

    Refactor new.js to implement HeadingComponent and TitleComponent; update rendering logic to use component composition.

--- new.js
+++ new.js
@@ -10,12 +10,34 @@ const heading = React.createElement("h1", {id: "heading"}, "hi from React instal
 //This is a react element using JSX syntax
 
 const jsxHeading = <h1>hi from ReactJSX</h1>
-//Jsx => is converted into React Element by Babel
 
-//react element is then renderd on browser as a DOM element
-//than DOM element is rendered on the browser as HTML element by react-dom
+
+//Noe we will create a React Component
+// a js function nothing more: 
+// to create a react componenet we cansee that they should be in a 
+// capital leter so that JSX and identitfy that now we have create a funct ion component
+const TitleComponent = () => (
+        <h1>Hi, I am Title</h1>
+);
+
+
+//Now we will create a component that will use the TitleComponent
+// this is a functional component using another functional component
+//making a component composition
+const HeadingComponent = () => (
+    <h1>Hi i am Heading Component that holds title: <TitleComponent /></h1>
+);
+
+
+//Now we will render the component to the root div
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
-root.render(jsxHeading);
+// this is how we can render a react component(funtuional component)figma
+ 
+root.render(<HeadingComponent />);
+
+
+
+
 
